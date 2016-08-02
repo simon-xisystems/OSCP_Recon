4c6ff907a4a884a7eb21cf2e6eb99e8265486c7c
simon-xisystems <simon@xisystems.co.uk>
2016-08-02 12:24:58 +0100

    Add files via upload
    modified scan script in order to pull out IP, host name and Port numbers in CSVish format for analysis from Nmap pushes to fixed .txt file

---
+++ scanreportv1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function usage {
+	echo "Usage: $1 [-f nmap.grepable] [-i IP] [-p port] [-s service] [-P protocol] [-a]"
+	echo "  -a: outputs matching IP addresses only"
+}
+
+db=""
+ip=""
+port=""
+proto=""
+addressonly=""
+while getopts "f:i:p:P:s:a" OPT; do
+	case $OPT in
+		f) db=$OPTARG;;
+		i) ip=$OPTARG;;
+		p) port=$OPTARG;;
+		s) service=$OPTARG;;
+		P) proto=$OPTARG;;
+		a) addressonly=true;;
+		*) usage $0; exit;;
+	esac
+done
+
+if [[ -z $db ]]; then 
+	# check if nmap-db.grep exists
+	if [[ -f ${HOME}/nmap-db.grep ]]; then 
+		db=${HOME}/nmap-db.grep
+	else
+		usage $0
+		exit
+	fi
+fi
+
+if [[ ! -z $ip ]]; then # search by IP
+	r=$(grep -w "$ip" "$db" | grep -v ^# | sed 's/Ports: /\'$'\n/g' |  tr '/' '\t' | tr ',' '\n' | sed 's/^ //g' | grep -v "Status: Up" | sed 's/Host:/\\033[0;32mHost:\\033[0;39m/g' | sed 's/Ignored State.*$//')
+
+elif [[ ! -z $port ]]; then # search by port number
+	r=$(grep -w -E -e "($port)\/open" "$db" | grep -v ^# | sed 's/Ports: /\'$'\n/g' |  tr '/' '\t' | tr ',' '\n' | sed 's/^ //g' | grep -v "Status: Up" | grep -E -e "Host: " -e "^(${port})" | sed 's/Host:/\\033[0;32mHost:\\033[0;39m/g' | sed 's/Ignored State.*$//') 
+
+elif [[ ! -z $service ]]; then # search by service name
+	r=$(grep -w -E -i -e "($service)" "$db" | grep -v ^# | sed 's/Ports: /\'$'\n/g' |  tr '/' '\t' | tr ',' '\n' | sed 's/^ //g' | grep -v "Status: Up" | grep -i -E -e "Host: " -e "(${service})" | sed 's/Host:/\\033[0;32mHost:\\033[0;39m/g' | sed 's/Ignored State.*$//')
+elif [[ ! -z $proto ]]; then
+	r=$(grep -w -E -i -e "($proto)" "$db" | grep -v ^# | sed 's/Ports: /\'$'\n/g' |  tr '/' '\t' | tr ',' '\n' | sed 's/^ //g' | grep -v "Status: Up" | grep -i -E -e "Host: " -e "(${proto})" | sed 's/Host:/\\033[0;32mHost:\\033[0;39m/g' | sed 's/Ignored State.*$//')
+else
+	r=$(cat "$db" | grep -v ^# | sed 's/Ports: /\'$'\n/g' | tr '/' '\t' | tr ',' '\n' | sed 's/^ //g' | grep -v "Status: Up" | sed 's/Ignored State.*$//'|awk {'print $1, $2, $3, $4'} | tr " " "," > hostname_port.txt)
+fi
+
+if [[ $addressonly ]]; then # output only IPs/hostnames
+	echo -e "$r" | grep "Host:" | awk {'print $2'} 
+else
+	echo -e "$r"
+fi
